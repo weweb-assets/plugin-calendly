566ac0e7630ab843970b1e4042111ac22e4cebf8
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:18:10 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -1,5 +1,20 @@
 export default {
     editor: {
         designSystemId: '0b17927a-d1ba-490b-8f26-ec9b341a8b86',
-    },
+        /* wwEditor:start */
+        settings: {
+            copilot: {
+                description: 'Configure the Calendly plugin settings',
+                returns: 'void',
+                schema: {
+                    designSystemId: {
+                        type: 'string',
+                        description: 'The unique identifier for the design system',
+                        bindable: false
+                    }
+                }
+            }
+        }
+        /* wwEditor:end */
+    }
 };
